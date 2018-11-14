a153a9ca2f3922f155987cbd590c62fee99c4b37
Hristo Anastasov <HAnastasov@infragistics.com>
2018-11-14 15:04:41 +0200

    Initial commit for project: POC_XPO

---
+++ e2e/src/app.e2e-spec.ts
@@ -0,0 +1,19 @@
+import { AppPage } from './app.po';
+
+describe('POC_XPO App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display project name in navigation', () => {
+    page.navigateTo();
+    expect(page.getNavText()).toEqual('POC_XPO');
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getWelcomeText()).toEqual('Welcome to Ignite UI for Angular!');
+  });
+});
